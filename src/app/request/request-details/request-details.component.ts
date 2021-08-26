968b83d7ee49e5043ef875b48483236a34f65e7b
Marc Serret <vesperon51@gmail.com>
2021-08-26 10:17:38 +0200

    removed console.log

--- src/app/request/request-details/request-details.component.ts
+++ src/app/request/request-details/request-details.component.ts
@@ -85,8 +85,6 @@ export class RequestDetailsComponent implements OnInit, OnDestroy {
     this.applicationService.getByName(this.request['classs']).subscribe(
       res => {
         this.application = res;
-        console.log(this.application);
-
         if (!this.application.applicationConstraint) {
           alert("Constraint not setted ATM");
         } else {
