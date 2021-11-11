c6765b6877d9dd8d3355f868f116d74223bf2041
Marc Serret <vesperon51@gmail.com>
2021-11-11 11:23:42 +0100

    Added more application constraints

--- src/app/models/ApplicationConstraint.ts
+++ src/app/models/ApplicationConstraint.ts
@@ -10,10 +10,13 @@ export class ApplicationConstraint {
   showQuantificationHeatMap: boolean;
   showDendogram: boolean;
   showChargesPlot: boolean;
+  showHistonesBiological: boolean;
+  showHistonesTailored: boolean;
+
 
   constructor(id: number, name: string, showFileInfoPlot: boolean, showIdentifiedPeptidesPlot: boolean,
     showIdentifiedProteinsPlot: boolean, showModificationsPlot: boolean, showQuantificationAndContaminantList: boolean,
-    showQuantificationHeatMap: boolean, showDendogram: boolean, showChargesPlot: boolean) {
+    showQuantificationHeatMap: boolean, showDendogram: boolean, showChargesPlot: boolean, showHistonesBiological: boolean, showHistonesTailored: boolean) {
     this.id = id;
     this.name = name;
     this.showFileInfoPlot = showFileInfoPlot;
@@ -24,5 +27,7 @@ export class ApplicationConstraint {
     this.showQuantificationHeatMap = showQuantificationHeatMap;
     this.showDendogram = showDendogram;
     this.showChargesPlot = showChargesPlot;
+    this.showHistonesBiological = showHistonesBiological;
+    this.showHistonesTailored = showHistonesTailored;
   }
 }
