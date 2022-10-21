b2c5fd59617f37e2b754e47b470782ed69bccefd
Toni Hermoso Pulido <toniher@cau.cat>
2022-10-21 19:36:43 +0200

    allowing retrieval of config

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,5 +2,22 @@
   window['env'] = window['env'] || {};
 
   // Environment variables
-  window['env']['apiPrefix'] = 'http://qsample.crg.eu/';
+  window['env']['apiPrefix'] = 'http://localhost:8099/';
+
+  let config_url = window['env']['apiPrefix']+"api/config";
+
+  fetch(config_url)
+  .then(res => res.json())
+  .then(out => {
+      if ( out.hasOwnProperty('local_requests') ) {
+        window['env']['local_requests'] = out['local_requests'];
+      }
+      if ( out.hasOwnProperty('qcloud2_disable') ) {
+        window['env']['qcloud2_disable'] = out['qcloud2_disable'];
+      }
+      if ( out.hasOwnProperty('nextflow_tower_disable') ) {
+        window['env']['nextflow_tower_disable'] = out['nextflow_tower_disable'];
+      }
+    }
+  ).catch(err => { throw err });
 })(this)
--- src/assets/env.sample.js
+++ src/assets/env.sample.js
@@ -3,5 +3,23 @@
 
   // Environment variables
   window['env']['apiPrefix'] = '${QSAMPLE_API_PREFIX}';
+
+  let config_url = window['env']['apiPrefix']+"api/config";
+
+  fetch(config_url)
+  .then(res => res.json())
+  .then(out => {
+      if ( out.hasOwnProperty('local_requests') ) {
+        window['env']['local_requests'] = out['local_requests'];
+      }
+      if ( out.hasOwnProperty('qcloud2_disable') ) {
+        window['env']['qcloud2_disable'] = out['qcloud2_disable'];
+      }
+      if ( out.hasOwnProperty('nextflow_tower_disable') ) {
+        window['env']['nextflow_tower_disable'] = out['nextflow_tower_disable'];
+      }
+    }
+  ).catch(err => { throw err });
+
 })(this);
 
