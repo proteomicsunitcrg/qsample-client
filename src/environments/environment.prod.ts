9d69466bed3ecb510fed9ceeef4cd69ad7788dfb
Toni Hermoso Pulido <toniher@cau.cat>
2022-10-21 19:50:19 +0200

    environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiPrefix: window['env']['apiPrefix'] || 'http://qsample.crg.eu/',
-  local_requests: window['env']['local_requests'] || false
-  nextflow_tower_disable: window['env']['nextflow_tower_disable'] || false
+  local_requests: window['env']['local_requests'] || false,
+  nextflow_tower_disable: window['env']['nextflow_tower_disable'] || false,
   qcloud2_disable: window['env']['qcloud2_disable'] || false
 };
