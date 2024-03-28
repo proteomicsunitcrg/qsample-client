2f000bd008e5afb271e14cce5145ad52901ef44f
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2024-03-28 12:49:28 +0100

    Removing user manager with non local mode

--- src/app/settings/settings-main/settings-main.component.ts
+++ src/app/settings/settings-main/settings-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerConfigService } from '../../services/serverconfig.service';
 
 @Component({
   selector: 'app-settings-main',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsMainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private serverConfigService: ServerConfigService
+  ) {}
+
+  local: boolean = true;
 
   ngOnInit(): void {
+
+    this.getServerConfig();
   }
+
+  private getServerConfig() {
+    this.serverConfigService.getConfig().subscribe(
+      (res) => {
+        if (res.hasOwnProperty('local_requests')) {
+          this.local = res['local_requests'];
+        }
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
 
 }
