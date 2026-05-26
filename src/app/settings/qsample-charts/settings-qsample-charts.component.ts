e235f1436ea37e65cc67fb3d90ed2ff5d40e89c0
Roger Olivella <roger.olivella@crg.eu>
2026-05-26 11:13:30 +0200

    Add QSample chart settings entry and applications list

--- src/app/settings/qsample-charts/settings-qsample-charts.component.ts
+++ src/app/settings/qsample-charts/settings-qsample-charts.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+
+import { Application } from '../../models/Application';
+import { ApplicationService } from '../../services/application.service';
 
 @Component({
   selector: 'app-settings-qsample-charts',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsQsampleChartsComponent implements OnInit {
 
-  constructor() { }
+  columnsToDisplay = ['name', 'action'];
+  dataSource: MatTableDataSource<Application>;
+
+  constructor(
+    private applicationService: ApplicationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getAllApplications();
+  }
+
+  private getAllApplications(): void {
+    this.applicationService.getAll().subscribe(
+      res => {
+        this.dataSource = new MatTableDataSource(res);
+      },
+      err => {
+        console.error(err);
+      }
+    );
+  }
+
+  public editCharts(application: Application): void {
+    this.router.navigate([
+      '/settings/qsample/charts/application',
+      application.id
+    ]);
   }
 
 }
