d6e26312480766b598fc7e845168bcb63aae1461
Roger Olivella <roger.olivella@crg.eu>
2026-06-09 10:49:04 +0200

    Redesign sample preparation QC layout

--- src/app/wetlab/wetlab-main/wetlab-main.component.ts
+++ src/app/wetlab/wetlab-main/wetlab-main.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { WetLab } from '../../models/WetLab';
+import { WetLabCategory } from '../../models/WetLabCategory';
+import { WetLabService } from '../../services/wetlab.service';
 
 @Component({
   selector: 'app-wetlab-main',
@@ -8,9 +11,53 @@ import { Routes, RouterModule } from '@angular/router';
 })
 export class WetlabMainComponent implements OnInit {
 
-  constructor() { }
+  WetLabs: WetLab[];
+  Categories: WetLabCategory[];
+  title = 'Sample preparation QC';
+
+  constructor(
+    private wetLabService: WetLabService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getAllWetlabs();
   }
 
+  private getAllWetlabs(): void {
+    this.wetLabService.getWetlabLists().subscribe(
+      (res) => {
+        this.WetLabs = res;
+        this.Categories = this.processCategories(this.WetLabs);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  private processCategories(wetlab: WetLab[]): WetLabCategory[] {
+    const wetlabArray: WetLabCategory[] = [];
+
+    for (const item of wetlab) {
+      let found = 0;
+
+      for (const exist of wetlabArray) {
+        if (item.wetlabCategory.id === exist.id) {
+          found = 1;
+        }
+      }
+
+      if (found === 0) {
+        wetlabArray.push(item.wetlabCategory);
+      }
+    }
+
+    wetlabArray.sort((a, b) => a.id - b.id);
+    return wetlabArray;
+  }
+
+  public navigate(wetLab: WetLab): void {
+    this.router.navigate(['/wetlab/plot', wetLab.apiKey]);
+  }
 }
